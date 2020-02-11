2a7bc3e3aeeab66c3e327550a223546d0dfbab15
Freeze-Shi <shij134@newschool.edu>
2020-02-11 09:29:20 -0500

    s

---
+++ project1/arrests/script.js
@@ -0,0 +1,208 @@
+
+
+    $( function() {
+      $( "#draggable"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable01"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable02"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable03"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable04"  ).draggable();
+    } );
+
+  
+    $( function() {
+      $( "#draggable2" ).draggable();
+
+    } );
+     $( function() {
+      $( "#draggable21"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable22"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable23"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable24"  ).draggable();
+    } );
+
+
+    $( function() {
+      $( "#draggable3" ).draggable();
+    } );
+    $( function() {
+      $( "#draggable31"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable32"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable33"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable34"  ).draggable();
+    } );
+
+
+
+    $( function() {
+      $( "#draggable4" ).draggable();
+    } );
+     $( function() {
+      $( "#draggable41"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable42"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable43"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable44"  ).draggable();
+    } );
+
+
+
+    $( function() {
+      $( "#draggable5" ).draggable();
+    } );
+    $( function() {
+      $( "#draggable51"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable52"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable53"  ).draggable();
+    } );
+    $( function() {
+      $( "#draggable54"  ).draggable();
+    } );
+
+     $( function() {
+      $( "#draggable6" ).draggable();
+    } );
+     $( function() {
+      $( "#draggable61" ).draggable();
+    } );
+     $( function() {
+      $( "#draggable62" ).draggable();
+    } );
+     $( function() {
+      $( "#draggable63" ).draggable();
+    } );
+     $( function() {
+      $( "#draggable64" ).draggable();
+    } );
+
+      $( function() {
+      $( "#draggable7" ).draggable();
+    } );
+      $( function() {
+      $( "#draggable71" ).draggable();
+    } );
+      $( function() {
+      $( "#draggable72" ).draggable();
+    } );
+      $( function() {
+      $( "#draggable73" ).draggable();
+    } );
+      $( function() {
+      $( "#draggable74" ).draggable();
+    } );
+
+
+
+
+       $( function() {
+      $( "#draggable8" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable81" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable82" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable83" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable84" ).draggable();
+    } );
+
+        $( function() {
+      $( "#draggable9" ).draggable();
+    } );
+        $( function() {
+      $( "#draggable91" ).draggable();
+    } );
+        $( function() {
+      $( "#draggable92" ).draggable();
+    } );
+        $( function() {
+      $( "#draggable93" ).draggable();
+    } );
+        $( function() {
+      $( "#draggable94" ).draggable();
+    } );
+
+
+         $( function() {
+      $( "#draggable10" ).draggable();
+    } );
+         $( function() {
+      $( "#draggable101" ).draggable();
+    } );
+         $( function() {
+      $( "#draggable102" ).draggable();
+    } );
+         $( function() {
+      $( "#draggable103" ).draggable();
+    } );
+         $( function() {
+      $( "#draggable104" ).draggable();
+    } );
+
+      $( function() {
+      $( "#draggable11" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable111" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable112" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable113" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable114" ).draggable();
+    } );
+       $( function() {
+      $( "#draggable115" ).draggable();
+    } );
+
+       $( function() {
+      $( "#draggable99" ).draggable();
+    } );
+
+       $(window).load(function () {
+      $(".trigger_popup_fricc").click(function(){
+         $('.hover_bkgr_fricc').show();
+      });
+      $('.hover_bkgr_fricc').click(function(){
+          $('.hover_bkgr_fricc').hide();
+      });
+      $('.popupCloseButton').click(function(){
+          $('.hover_bkgr_fricc').hide();
+      });
+  });
+    
