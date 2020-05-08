1c2b0675c28a9a45d8e583b479510b62ac3b6616
Freeze-Shi <shij134@newschool.edu>
2020-05-08 11:48:52 +0800

    finalupdate

--- assets/js.js
+++ assets/js.js
@@ -18,6 +18,8 @@ $("#close").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifthtext").css("display","none")
 	$("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
   });
 
 
@@ -28,6 +30,8 @@ $("#first").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifhttext").css("display","none")
 	$("#sixthtext").css("display","none")
+   $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
 
     $("#interface").css("display","inline")
 
@@ -37,6 +41,8 @@ $("#first").on("click",function(){
     $("#fourthimg").css("display","none")
     $("#fifthimg").css("display","none")
     $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
   });
 
 $("#second").on("click",function(){
@@ -46,6 +52,8 @@ $("#second").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifhttext").css("display","none")
 	$("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
 
     $("#interface").css("display","inline")
     $("#secondimg").css("display","inline")
@@ -54,6 +62,8 @@ $("#second").on("click",function(){
     $("#fourthimg").css("display","none")
     $("#fifthimg").css("display","none")
     $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
   });
 
 $("#third").on("click",function(){
@@ -63,6 +73,9 @@ $("#third").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifhttext").css("display","none")
 	$("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
+
     $("#interface").css("display","inline")
     $("#thirdimg").css("display","inline")
     $("#firstimg").css("display","none")
@@ -70,6 +83,8 @@ $("#third").on("click",function(){
     $("#fourthimg").css("display","none")
     $("#fifthimg").css("display","none")
     $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
   });
 
 $("#fourth").on("click",function(){
@@ -79,6 +94,9 @@ $("#fourth").on("click",function(){
 	$("#fourthtext").css("display","inline")
 	$("#fifhttext").css("display","none")
 	$("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
+
     $("#interface").css("display","inline")
     $("#fourthimg").css("display","inline")
      $("#firstimg").css("display","none")
@@ -86,6 +104,8 @@ $("#fourth").on("click",function(){
      $("#thirdimg").css("display","none")
     $("#fifthimg").css("display","none")
     $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
   });
 
 $("#fifth").on("click",function(){
@@ -95,6 +115,8 @@ $("#fifth").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifthtext").css("display","inline")
 	$("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
 
     $("#interface").css("display","inline")
     $("#fifthimg").css("display","inline")
@@ -103,6 +125,8 @@ $("#fifth").on("click",function(){
      $("#thirdimg").css("display","none")
     $("#fourthimg").css("display","none")
     $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
   });
 
 $("#sixth").on("click",function(){
@@ -112,6 +136,9 @@ $("#sixth").on("click",function(){
 	$("#fourthtext").css("display","none")
 	$("#fifhttext").css("display","none")
 	$("#sixthtext").css("display","inline")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","none")
+
     $("#interface").css("display","inline")
     $("#sixthimg").css("display","inline")
      $("#firstimg").css("display","none")
@@ -119,8 +146,56 @@ $("#sixth").on("click",function(){
      $("#thirdimg").css("display","none")
     $("#fourthimg").css("display","none")
     $("#fifthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","none")
 
   });
+
+  $("#seventh").on("click",function(){
+  $("#firsttext").css("display","none")
+  $("#secondtext").css("display","none")
+  $("#thirdtext").css("display","none")
+  $("#fourthtext").css("display","none")
+  $("#fifhttext").css("display","none")
+  $("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","inline")
+  $("#eighthtext").css("display","none")
+  
+    $("#interface").css("display","inline")
+    $("#sixthimg").css("display","none")
+     $("#firstimg").css("display","none")
+    $("#secondimg").css("display","none")
+     $("#thirdimg").css("display","none")
+    $("#fourthimg").css("display","none")
+    $("#fifthimg").css("display","none")
+    $("#seventhimg").css("display","inline")
+    $("#eighthimg").css("display","none")
+
+  });
+
+  $("#eighth").on("click",function(){
+  $("#firsttext").css("display","none")
+  $("#secondtext").css("display","none")
+  $("#thirdtext").css("display","none")
+  $("#fourthtext").css("display","none")
+  $("#fifhttext").css("display","none")
+  $("#sixthtext").css("display","none")
+  $("#seventhtext").css("display","none")
+  $("#eighthtext").css("display","inline")
+  
+    $("#interface").css("display","inline")
+     $("#firstimg").css("display","none")
+    $("#secondimg").css("display","none")
+     $("#thirdimg").css("display","none")
+    $("#fourthimg").css("display","none")
+    $("#fifthimg").css("display","none")
+    $("#sixthimg").css("display","none")
+    $("#seventhimg").css("display","none")
+    $("#eighthimg").css("display","inline")
+
+  });
+  
+
  /* $("#start").on("click",function(){
     $("#first").css("display","none")
     $("#mouth").css("display","none")
